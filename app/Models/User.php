ad41a1bf88d78b11bb80077552aa20839ff8fa0c
kevinjiao <kevin.jiao@youyoutingche.com>
2018-07-05 02:04:41 +0000

    Move user model to the new models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,11 +9,14 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    /*过滤用户提交的字段，只有包含在该属性中的字段才能够被正常更新，避免批量赋值的错误*/
     protected $fillable = [
         'name', 'email', 'password',
     ];
@@ -23,6 +26,7 @@ class User extends Authenticatable
      *
      * @var array
      */
+    /*该字段在需要对用户密码或其它敏感信息在用户实例通过数组或 JSON 显示时进行隐藏*/
     protected $hidden = [
         'password', 'remember_token',
     ];
